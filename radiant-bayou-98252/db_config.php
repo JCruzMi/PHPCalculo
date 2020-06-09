32ead2281bd187feb8d1260a4d15a5b39935441d
JCruzMi <47216265+JCruzMi@users.noreply.github.com>
2020-06-09 18:38:07 -0500

    n

---
+++ radiant-bayou-98252/db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "yijldbnhldkdip"); // Usuario
+define('DB_PASSWORD', "6e1ea1fd965c3e5fbae57e93aa637da6e2b4a80e38655327502edffb6b1f848b"); // Password
+define('DB_DATABASE', "d6rivhgv6q185a"); // Nombre de la base de datos
+define('DB_SERVER', "ec2-3-222-30-53.compute-1.amazonaws.com"); //host server
+?>
---
+++ radiant-bayou-98252/index.php
@@ -0,0 +1,3 @@
+<?php
+    echo "prueba zzzzzzZzZzzzZZZzzz";
+?>
